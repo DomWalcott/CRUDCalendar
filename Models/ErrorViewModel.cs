a810d05b9dbdb07f5f0a72fd0f25abd55d9914ed
Domdidly <Domdidly@DOMDIDLY-PC>
2026-06-16 21:39:57 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUDCalendar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
