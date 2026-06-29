d4057f2b94f364a7225d3dfcce088adf6a9fc6a1
Dominic Walcott <d.walcott85@outlook.com>
2026-06-29 23:03:52 +0100

    did more work on the TaskItemData model. Added the constructor and the AddTask method.

--- Models/TaskItemData.cs
+++ Models/TaskItemData.cs
@@ -2,5 +2,24 @@
 {
     public class TaskItemData
     {
+        public DateOnly _taskDate;
+        public Dictionary<DateOnly, Dictionary<TimeOnly, string>> _taskList = new();
+
+
+        public TaskItemData(DateOnly taskDate)
+        {
+            _taskDate = taskDate;
+
+
+        }
+        public void AddTask(TimeOnly taskTime,string taskToBeAdded)
+        {
+            if (!_taskList.ContainsKey(_taskDate))
+            {
+                _taskList[_taskDate] = new Dictionary<TimeOnly, string>();
+            }
+            _taskList[_taskDate][taskTime] = taskToBeAdded;
+        }
     }
+    
 }
